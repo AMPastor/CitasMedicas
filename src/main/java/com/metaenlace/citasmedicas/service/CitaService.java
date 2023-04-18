4187ee8b52479313e8e23e57682fb1cc18946684
AMPastor <107792373+AMPastor@users.noreply.github.com>
2023-04-18 11:13:18 +0200

    Update

--- src/main/java/com/metaenlace/citasmedicas/service/CitaService.java
+++ src/main/java/com/metaenlace/citasmedicas/service/CitaService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Optional;
-import java.util.Collections;
 import java.util.List;
 
 import static com.metaenlace.citasmedicas.mapper.CitaMapper.INSTANCE;
@@ -29,6 +28,8 @@ public class CitaService {
     @Autowired
     private MedicoRepository medicoRepository;
 
+    //Mostrar Citas
+    //El método findAll devuelve una lista de objetos CitaDTO (List<CitaDTO>).
     public List<CitaDTO> findAll(){
         List<CitaDTO> lista = new ArrayList<>();
         Iterable<Cita> listaCitas = citaRepository.findAll();
@@ -39,22 +40,26 @@ public class CitaService {
         return lista;
     }
 
+    //Mostrar Citas por ID
+    //El método findById devuelve un objeto CitaDTO (CitaDTO).
     public CitaDTO findById(long id){
         Cita cita = citaRepository.findById(id).orElse(null);
         if(cita == null){
-            throw new NotFoundException("La cita no existe");
+            throw new NotFoundException("No existe ninguna cita.");
         }
         return INSTANCE.citaToCitaDTO(cita);
     }
 
+    //Guardar citas
+    //El método save devuelve un objeto CitaDTO (CitaDTO).
     public CitaDTO save(CitaDTO citaDTO) {
         Paciente paciente = pacienteRepository.findById(citaDTO.getPacienteId()).orElse(null);
         if (paciente == null) {
-            throw new NotFoundException("Paciente no encontrado");
+            throw new NotFoundException("No se ha encontrado ningún paciente.");
         }
         Medico medico = medicoRepository.findById(citaDTO.getMedicoId()).orElse(null);
         if (medico == null) {
-            throw new NotFoundException("Médico no encontrado");
+            throw new NotFoundException("No se ha encontrado ningún paciente médico.");
         }
         Cita cita = new Cita();
         // Asigna los valores de los atributos de la entidad cita
@@ -71,24 +76,29 @@ public class CitaService {
         return savedCitaDTO;
     }
 
+    //Actualizar citas
+    //El método update devuelve un objeto Cita (Cita).
     public Cita update(Long id, CitaDTO citaDTO){
         Optional<Paciente> pacienteOpt = pacienteRepository.findById(citaDTO.getPacienteId());
         if(pacienteOpt.isEmpty()){
-            throw new NotFoundException("Paciente no encontrado");
+            throw new NotFoundException("No se ha encontrado ningún paciente.");
         }
         Optional<Medico> medicoOpt = medicoRepository.findById(citaDTO.getMedicoId());
         if(medicoOpt.isEmpty()){
-            throw new NotFoundException("Médico no encontrado");
+            throw new NotFoundException("No se ha encontrado ningún médico.");
         }
         Cita cita = INSTANCE.citaDTOToCita(citaDTO);
         cita.setId(id);
         return citaRepository.save(cita);
     }
 
+    //Borrar citas por id
+    //El método deleteById no devuelve nada (void).
+    //El tipo de retorno en este método se omite y se utiliza la palabra clave void para indicar que el método no devuelve ningún valor.
     public void deleteById(long id){
         Optional<Cita> cita = citaRepository.findById(id);
         if(cita.isEmpty()){
-            throw new NotFoundException("Cita no encontrada");
+            throw new NotFoundException("No se ha encontrado ninguna cita.");
         } else {
             citaRepository.deleteById(id);
         }
--- src/main/java/com/metaenlace/citasmedicas/service/PacienteService.java
+++ src/main/java/com/metaenlace/citasmedicas/service/PacienteService.java
@@ -7,78 +7,26 @@ import com.metaenlace.citasmedicas.mapper.PacienteMapper;
 import com.metaenlace.citasmedicas.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-/*
 @Service
 public class PacienteService {
 
     @Autowired
     private PacienteRepository pacienteRepository;
 
-    @Autowired
-    private PacienteMapper pacienteMapper;
-
-    public List<PacienteDTO> findAll() {
-        List<PacienteDTO> pacientesDTO = new ArrayList<>();
-        pacienteRepository.findAll().forEach(paciente -> pacientesDTO.add(pacienteMapper.pacienteToPacienteDTO(paciente)));
-        return pacientesDTO;
-    }
-
-    public Optional<PacienteDTO> findById(Long id) {
-        Optional<Paciente> paciente = pacienteRepository.findById(id);
-        if (paciente.isPresent()) {
-            return Optional.of(pacienteMapper.pacienteToPacienteDTO(paciente.get()));
-        } else {
-            throw new NotFoundException("Paciente no encontrado");
-        }
-    }
-
-    public PacienteDTO save(PacienteDTO pacienteDTO) {
-        Paciente paciente = pacienteMapper.pacienteDTOToPaciente(pacienteDTO);
-        return pacienteMapper.pacienteToPacienteDTO(pacienteRepository.save(paciente));
-    }
-
-    public PacienteDTO update(Long id, PacienteDTO pacienteDTO) {
-        Optional<Paciente> pacienteToUpdate = pacienteRepository.findById(id);
-        if (pacienteToUpdate.isPresent()) {
-            Paciente paciente = pacienteToUpdate.get();
-            paciente.setNombre(pacienteDTO.getNombre());
-            paciente.setApellidos(pacienteDTO.getApellidos());
-            paciente.setUsuario(pacienteDTO.getUsuario());
-            paciente.setClave(pacienteDTO.getClave());
-            paciente.setNSS(pacienteDTO.getNss());
-            paciente.setNumTarjeta(pacienteDTO.getNumTarjeta());
-            paciente.setTelefono(pacienteDTO.getTelefono());
-            paciente.setDireccion(pacienteDTO.getDireccion());
-            return pacienteMapper.pacienteToPacienteDTO(pacienteRepository.save(paciente));
-        } else {
-            throw new NotFoundException("Paciente no encontrado");
-        }
-    }
-
-
-    public void deleteById(Long id) {
-        pacienteRepository.deleteById(id);
-    }
-
-}*/
-
-@Service
-public class PacienteService {
-
-    @Autowired
-    private PacienteRepository pacienteRepository;
-
+    //El método findAll devuelve una lista de objetos PacienteDTO
+    // obtenidos a partir de todos los pacientes en la base de datos.
     public List<PacienteDTO> findAll() {
         List<PacienteDTO> pacientesDTO = new ArrayList<>();
         pacienteRepository.findAll().forEach(paciente -> pacientesDTO.add(PacienteMapper.INSTANCE.pacienteToPacienteDTO(paciente)));
         return pacientesDTO;
     }
 
+    //El método findById devuelve un objeto Optional<PacienteDTO> que representa al
+    // paciente con el ID proporcionado o lanza una excepción NotFoundException.
     public Optional<PacienteDTO> findById(Long id) {
         Optional<Paciente> paciente = pacienteRepository.findById(id);
         if (paciente.isPresent()) {
@@ -88,11 +36,15 @@ public class PacienteService {
         }
     }
 
+    //El método save guarda un nuevo paciente en la base de datos y lo devuelve convertido
+    // en un objeto PacienteDTO
     public PacienteDTO save(PacienteDTO pacienteDTO) {
         Paciente paciente = PacienteMapper.INSTANCE.pacienteDTOToPaciente(pacienteDTO);
         return PacienteMapper.INSTANCE.pacienteToPacienteDTO(pacienteRepository.save(paciente));
     }
 
+    //El método update actualiza un paciente existente en la base de datos con los valores proporcionados
+    //y devuelve el paciente actualizado convertido en un objeto PacienteDTO.
     public PacienteDTO update(Long id, PacienteDTO pacienteDTO) {
         Optional<Paciente> pacienteToUpdate = pacienteRepository.findById(id);
         if (pacienteToUpdate.isPresent()) {
@@ -111,6 +63,7 @@ public class PacienteService {
         }
     }
 
+    //El método deleteById elimina un paciente de la base de datos utilizando el ID proporcionado.
     public void deleteById(Long id) {
         pacienteRepository.deleteById(id);
     }
