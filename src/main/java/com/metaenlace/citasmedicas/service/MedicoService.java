1769b51646cf16e4a7657536dd63ec543262c477
AMPastor <107792373+AMPastor@users.noreply.github.com>
2023-04-18 11:15:09 +0200

    Update

--- src/main/java/com/metaenlace/citasmedicas/service/MedicoService.java
+++ src/main/java/com/metaenlace/citasmedicas/service/MedicoService.java
@@ -1,4 +1,3 @@
-
 package com.metaenlace.citasmedicas.service;
 
 import com.metaenlace.citasmedicas.entity.Medico;
@@ -12,56 +11,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-/* No estaba usando INSTANCE
-@Service
-public class MedicoService {
-
-    @Autowired
-    private MedicoRepository medicoRepository;
-
-    @Autowired
-    private MedicoMapper medicoMapper;
-
-    public List<MedicoDTO> findAll() {
-        List<MedicoDTO> medicosDTO = new ArrayList<>();
-        medicoRepository.findAll().forEach(medico -> medicosDTO.add(medicoMapper.medicoToMedicoDTO(medico)));
-        return medicosDTO;
-    }
-
-    public Optional<MedicoDTO> findById(Long id) {
-        Optional<Medico> medico = medicoRepository.findById(id);
-        if (medico.isPresent()) {
-            return Optional.of(medicoMapper.medicoToMedicoDTO(medico.get()));
-        } else {
-            throw new NotFoundException("Medico no encontrado");
-        }
-    }
-
-    public MedicoDTO save(MedicoDTO medicoDTO) {
-        Medico medico = medicoMapper.medicoDTOToMedico(medicoDTO);
-        return medicoMapper.medicoToMedicoDTO(medicoRepository.save(medico));
-    }
-
-    public MedicoDTO update(Long id, MedicoDTO medicoDTO) {
-        Optional<Medico> medicoToUpdate = medicoRepository.findById(id);
-        if (medicoToUpdate.isPresent()) {
-            Medico medico = medicoToUpdate.get();
-            medico.setNombre(medicoDTO.getNombre());
-            medico.setApellidos(medicoDTO.getApellidos());
-            medico.setUsuario(medicoDTO.getUsuario());
-            medico.setClave(medicoDTO.getClave());
-            medico.setNumColegiado(medicoDTO.getNumColegiado());
-            return medicoMapper.medicoToMedicoDTO(medicoRepository.save(medico));
-        } else {
-            throw new NotFoundException("Medico no encontrado");
-        }
-    }
-
-    public void deleteById(Long id) {
-        medicoRepository.deleteById(id);
-    }
-
-}*/
 
 @Service
 public class MedicoService {
